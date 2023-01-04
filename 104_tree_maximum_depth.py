e9b4d6de3f1093f227c817496fae02bb781b45ee
Mikhail Ozernov <ozzygogogo@gmail.com>
2023-01-04 17:47:40 +0200

    tree maximum depth

---
+++ 104_tree_maximum_depth.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root):
+        if root is None:
+            return 0
+        else:
+            left = self.maxDepth(root.left)
+            right = self.maxDepth(root.right)
+            if left > right:
+                return left + 1
+            else:
+                return right +1
